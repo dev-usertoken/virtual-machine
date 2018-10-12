24b909d342e9c44a723e5dcd2f0f58bd8803ad5a
Alex Davis <alex2006hw@gmail.com>
2018-10-12 19:29:15 +0000

    move-jobworker-to-own-repo

--- jobworker/src/jobComponents.js
+++
@@ -1,6 +0,0 @@
-import path from 'path';
-
-export const Jobs = {
-  SimpleComponent: path.resolve(path.join('jobs', 'SimpleComponent.js')),
-  aphrodite: path.resolve(path.join('jobs', 'aphrodite/index.js')),
-};
